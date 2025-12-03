02be00ce870c1a31589dd88e4223cf79a247e1a5
DerylTimothy <deryltimothy@gmail.com>
2025-12-03 07:43:08 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+// Pengaturan Koneksi Database
+$host = "localhost"; // Ganti jika host Anda berbeda
+$user = "root";      // Ganti dengan username MySQL Anda
+$pass = "";          // Ganti dengan password MySQL Anda
+$db = "db_profil";   // Ganti dengan nama database yang Anda buat
+
+// Buat koneksi dengan MySQLi
+$koneksi = new mysqli($host, $user, $pass, $db);
+
+// Periksa koneksi
+if ($koneksi->connect_error) {
+    die("Koneksi ke database gagal: " . $koneksi->connect_error);
+}
+?>
